ad41c1d743c3ba2cf68192c530848cfb135d097a
Rilind Hoxhaj <rilind@Rilinds-MacBook-Pro.local>
2020-08-26 14:13:05 +0200

    added home image

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  imagePath: string;
+
+  constructor() {
+    this.imagePath = '../../assets/img/todolist2.jpg';
+  }
+
+  ngOnInit(): void {}
+}
